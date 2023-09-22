772b2b37cce891b0696247ce8900b549dc5d7682
David Batranu <david.batranu@eaudeweb.ro>
2023-09-22 13:12:13 +0300

    Configurable proxy response header drop.

--- theme_helpers/theme_helpers/helpers.py
+++ theme_helpers/theme_helpers/helpers.py
@@ -2,12 +2,18 @@ import paste.proxy as proxy
 
 
 class NoCSPProxy(proxy.Proxy):
+    def __init__(self, *args, suppress_http_response_headers=(), **kwargs):
+        super().__init__(*args, **kwargs)
+        self.suppress_http_response_headers = [
+            x.lower() for x in suppress_http_response_headers if x
+        ]
+
     def __call__(self, environ, start_response):
         def wrap_start_response(status, headers, **kwargs):
             clean_headers = [
                 (name, value)
                 for name, value in headers
-                if name.lower() != "content-security-policy"
+                if name.lower() not in self.suppress_http_response_headers
             ]
             return start_response(status, clean_headers, **kwargs)
 
@@ -15,11 +21,19 @@ class NoCSPProxy(proxy.Proxy):
         return resp
 
 
-def make_proxy_helper(_, address, allowed_request_methods="", suppress_http_headers=""):
+def make_proxy_helper(
+    _,
+    address,
+    allowed_request_methods="",
+    suppress_http_headers="",
+    suppress_http_response_headers="",
+):
     allowed_request_methods = proxy.aslist(allowed_request_methods)
     suppress_http_headers = proxy.aslist(suppress_http_headers)
+    suppress_http_response_headers = proxy.aslist(suppress_http_response_headers)
     return NoCSPProxy(
         address,
         allowed_request_methods=allowed_request_methods,
         suppress_http_headers=suppress_http_headers,
+        suppress_http_response_headers=suppress_http_response_headers,
     )
